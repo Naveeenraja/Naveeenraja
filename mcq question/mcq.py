eadc642999c5c315e3e014a6449e27c020e46912
raja <naveenraja2510@gmail.com>
2023-01-03 23:08:30 +0530

    commit 1

--- mcq question/mcq.py
+++ mcq question/mcq.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect, url_for,session
 from flask_session import Session
 import data.mcqutil as mcq_utils
-file ="Naveeenraja\mcq question\studreg.json"
+file ="mcq question\studreg.json"
 file1 = "mcq.json"
 app=Flask(__name__)
 app.config["SECRET_KEY"]="somekey"
